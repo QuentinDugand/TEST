b79fbc088a84aa251440989ab4829bce3ce3cc41
QuentinDugand <Quentin.Dugand@iut-valence.fr>
2014-02-10 16:32:47 +0100

    Résumé
    Commit

---
+++ src/Bulb.java
@@ -0,0 +1,31 @@
+
+
+public class Bulb
+{
+public static final int DEFAULT_POWER=100;
+
+private final int power;
+
+public boolean islit;
+
+public Bulb (string[] args)
+{
+	
+}
+
+public Bulb (string[] args)
+{
+	this.power= 150;
+}
+
+switch (on)
+{
+ this.islit=true;
+}
+switch (off)
+{
+this.islit=false;
+}
+
+
+}
